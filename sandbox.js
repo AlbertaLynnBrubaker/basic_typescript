0be8606d1911323db4fcf7d2b258b89e8f1985ef
Alberta Lynn Brubaker <findingalberta@gmail.com>
2022-11-22 16:27:38 -0600

    end of lesson

---
+++ sandbox.js
@@ -0,0 +1,8 @@
+var character = 'sam';
+console.log(character);
+1;
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ sandbox.ts
@@ -0,0 +1,11 @@
+const character = 'sam'
+
+console.log(character)
+1
+const inputs = document.querySelectorAll('input')
+
+console.log(inputs)
+
+inputs.forEach(input => {
+  console.log(input)
+})
